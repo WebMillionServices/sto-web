9b55571066e1b8980b23762eb91b11363ccf4ace
Digitalchetan85 <chetankumar.nv@gmail.com>
2021-05-21 18:06:26 +0530

    a

--- mail.php
+++ mail.php
@@ -1,58 +1,40 @@
 <?php
+  $mail_to = 'chetankumar.nv@gmail.com'; // specify your email here
+
+    // Assigning data from the $_POST array to variables
+    $firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$phone = $_POST['phone'];
+    $email = $_POST['email'];
+	$message = $_POST['message'];
+
+    // Construct email subject
+    $subject = 'Contact Enquiry From ' . $firstname;
+
+    // Construct email body
+    $body_message = 'From:' .$firstname ;
+	$body_message = 'Phone:' .$phone ;
+    $body_message = 'E-mail:' .$email;
+	$body_message = 'Name:' .$firstname."\r\n" .$lastname."\r\n" .$phone."\r\n" .$email."\r\n" .$message ;
 
 
+    // Construct email headers
+    $headers = 'From: ' . $email . "\r\n" ;
+    $headers = 'Reply-To: ' . $email . "\r\n";
 
+    $mail_sent = mail($mail_to, $subject, $body_message, $headers);
 
-// Import PHPMailer classes into the global namespace
-// These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-// Load Composer's autoloader
-require 'vendor/autoload.php';
-
-// Instantiation and passing `true` enables exceptions
-$mail = new PHPMailer(true);
-
-try {
-    //Server settings
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'digitalchetan85@gmail.com';                     // SMTP username
-    $mail->Password   = 'csipmdjhoniovkxq';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
-    //Recipients
-    $mail->setFrom('digitalchetan85@gmail.com', 'DC');
-    $mail->addAddress('chetankumar.nv@gmail.com', 'Chetan Kumar N');     // Add a recipient
-    //$mail->addAddress('ellen@example.com');               // Name is optional
-    //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('cc@example.com');
-    //$mail->addBCC('bcc@example.com');
-
-    // Attachments
-    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'Enquiry Details';
-        $mail->Body    = '<html><body>';
-        $mail->Body   .= '<table rules="all" style="border-style: solid; border-color: #666;" cellpadding="10">';
-        $mail->Body   .= "<tr style='background: #eee;'><td><strong>Online Enquery:</strong> </td><td>" . 'Contact Page' . "</td></tr>";
-        $mail->Body   .= "<tr><td><strong>Name:</strong> </td><td>" . strip_tags($_POST['name']) . "</td></tr>";
-        $mail->Body   .= "<tr><td><strong>Email:</strong> </td><td>" . strip_tags($_POST['email']) . "</td></tr>";
-        $mail->Body   .= "<tr><td><strong>Phone:</strong> </td><td>" . strip_tags($_POST['phone']) . "</td></tr>";
-        $mail->Body   .= "<tr><td><strong>Message:</strong> </td><td>" . strip_tags($_POST['message']) . "</td></tr>";
-        $mail->Body   .= "</table>";
-        $mail->Body   .= "</body></html>";
-
-    $mail->send();
-    echo "<p class='text-center text-success'><strong>Thanks for contacting us. We will reach you soon.</strong></p>";
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-}
+    if ($mail_sent == true){ ?>
+        <script language="javascript" type="text/javascript">
+        alert('Thank you for the message. We will contact you shortly.');
+        window.location = 'contact.html';
+        </script>
+    <?php } else { ?>
+    <script language="javascript" type="text/javascript">
+        alert('Message not sent. Please, notify the site administrator anburaj.creativenuts@gmail.com');
+        window.location = 'contact.html';
+    </script>
+    <?php
+    }
+  
+?>
